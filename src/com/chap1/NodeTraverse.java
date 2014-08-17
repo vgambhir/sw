93aaa7be31caf9a8462612433c40ebf66dd0b0d1
vgambhir <vgambhir@homeaway.com>
2014-08-17 12:17:01 -0500

    Add class

--- src/com/chap1/NodeTraverse.java
+++ src/com/chap1/NodeTraverse.java
@@ -20,18 +20,58 @@ public class NodeTraverse {
 			this.val = val;
 		}
 
-		public boolean equals(Object o) {
-			if (null == o)
-				return false;
-
-			if (o.getClass() == this.getClass()) {
-
-				if (((Node) o).val.equals(this.val))
-					return true;
-			}
-			return false;
-
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + getOuterType().hashCode();
+			result = prime * result + ((val == null) ? 0 : val.hashCode());
+			return result;
 		}
+
+		@Override
+		public boolean equals(Object obj) {
+			//reference equal
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			// getClass compare
+			if (getClass() != obj.getClass())
+				return false;
+			Node other = (Node) obj;
+			
+			// checking outer class type
+			if (!getOuterType().equals(other.getOuterType()))
+				return false;
+			if (val == null) {
+				if (other.val != null)
+					return false;
+			} else if (!val.equals(other.val))
+				return false;
+			return true;
+		}
+
+		private NodeTraverse getOuterType() {
+			return NodeTraverse.this;
+		}
+
+//		public boolean equals(Object o) {
+//			if (null == o)
+//				return false;
+//
+//			if (o.getClass() == this.getClass()) {
+//
+//				if (((Node) o).val.equals(this.val))
+//					return true;
+//			}
+//			return false;
+//
+//		}
+		
+		
+		
+		
 
 	}
 
